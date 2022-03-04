bc16b95c0eb528c1e26e5473e676267264c9591f
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-03-03 23:25:15 -0300

    add info about product

--- src/bot.ts
+++ src/bot.ts
@@ -59,34 +59,25 @@ client.on('outOfStock', param => {
 	log(param);
 });
 
-client.on('stockRefilled', url => {
+client.on('stockRefilled', product => {
 	log('Stock refilled!');
 	const channel = client.channels.cache.find(
 		channel => (channel as TextChannel).name === notifyTextChannel
 	);
 	const exampleEmbed = new MessageEmbed()
-		.setColor('##f58442')
-		.setTitle('Some title')
-		.setURL('https://discord.js.org/')
+		.setColor('#f58442')
+		.setTitle(product.name)
+		.setURL(product.url)
 		.setAuthor({
-			name: 'Some name',
-			iconURL: 'https://i.imgur.com/AfFp7pu.png',
+			name: 'Estoque disponível!',
+			iconURL: product.store_image,
 			url: 'https://discord.js.org',
 		})
-		.setDescription('Some description here')
-		.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-		.addFields(
-			{ name: 'Regular field title', value: 'Some value here' },
-			{ name: '\u200B', value: '\u200B' },
-			{ name: 'Inline field title', value: 'Some value here', inline: true },
-			{ name: 'Inline field title', value: 'Some value here', inline: true }
-		)
-		.addField('Inline field title', 'Some value here', true)
-		.setImage('https://i.imgur.com/AfFp7pu.png')
+		.setDescription('Some description here :rocket:')
+		.setThumbnail(product.image_url)
 		.setTimestamp()
 		.setFooter({
 			text: 'Some footer text here',
-			iconURL: 'https://i.imgur.com/AfFp7pu.png',
 		});
 	// (channel as TextChannel).send(`Em estoque. Link: ${url}`);
 	(channel as TextChannel).send({ embeds: [exampleEmbed] });
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,15 @@ import { CronJob } from 'cron';
 import client from './bot';
 import { exitHandler } from './helpers';
 
-const url = 'https://www.nike.com.br/snkrs/air-jordan-1-153-169-211-351285';
+const product = {
+	name: 'Air Jordan 1	Patent Bred',
+	url: 'https://www.nike.com.br/snkrs/air-jordan-1-153-169-211-351285',
+	image_url:
+		'https://images.lojanike.com.br/1024x1024/produto/tenis-air-jordan-1-retro-high-og-masculino-555088-063-1-11637067284.jpg',
+	store: 'Nike',
+	store_image:
+		'https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/ba6c1527-a9c5-4efa-af58-0930b3a8aa34',
+};
 
 const configureBrowser = async () => {
 	const browser = await puppeteer.launch({
@@ -16,7 +24,7 @@ const configureBrowser = async () => {
 	await page.setUserAgent(
 		'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
 	);
-	await page.goto(url);
+	await page.goto(product.url);
 
 	return page;
 };
@@ -34,9 +42,10 @@ const isSoldOff = async (page: Page): Promise<boolean> => {
 const monitorStockAvailability = async (page: Page) => {
 	const soldOff = await isSoldOff(page);
 
+	// TODO change to !soldOff. Its this way for testing pourpouse
 	if (soldOff) {
 		console.log('NÃO ESTÁ MAIS ESGOTADO');
-		if (client.isReady()) client.emit('stockRefilled', url);
+		if (client.isReady()) client.emit('stockRefilled', product);
 	} else {
 		console.log('CONTINUA ESGOTADO');
 		client.emit('outOfStock', 'Out of stock');
@@ -44,9 +53,9 @@ const monitorStockAvailability = async (page: Page) => {
 };
 
 (async () => {
-	process.stdin.resume()
+	process.stdin.resume();
 
-	process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+	process.on('SIGINT', exitHandler.bind(null, { exit: true }));
 
 	let page = await configureBrowser();
 	const job = new CronJob(
