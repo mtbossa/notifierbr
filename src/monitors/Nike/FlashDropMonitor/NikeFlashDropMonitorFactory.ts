455958b794fe250c57eb7e492c94e8f40ec3deac
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-03-24 15:33:31 -0300

    fix not finding files

--- src/monitors/Nike/FlashDropMonitor/NikeFlashDropMonitorFactory.ts
+++ src/monitors/Nike/FlashDropMonitor/NikeFlashDropMonitorFactory.ts
@@ -1,14 +1,17 @@
 import { Client } from "discord.js";
 import fs from "fs";
+import path from "path";
 import NikeFlashDropsAPIRepository from "../repositories/implementations/NikeFlashDropsAPIRepository";
 import { NikeAPISearchRequest } from "../models/requests/NikeAPISearchRequest";
 import NikeFlashDropsMonitorService from "../services/NikeFlashDropMonitorService";
 import NikeFlashDropsMonitor from "./NikeFlashDropMonitor";
 
 export default function createNikeFlashDropMonitor(discordClient: Client) {
-  const requestsObjects = fs.readFileSync(
-    "../../../../scrape_data/monitors/Nike/searchs.json",
-    "utf-8",
+  const requestsObjects = JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, "../../../../scrape_data/monitors/Nike/searchs.json"),
+      "utf-8",
+    ),
   ) as unknown as NikeAPISearchRequest[];
 
   const nikeFlashDropService = new NikeFlashDropsMonitorService();
--- src/monitors/Nike/RestockMonitor/NikeRestockMonitorFactory.ts
+++ src/monitors/Nike/RestockMonitor/NikeRestockMonitorFactory.ts
@@ -1,15 +1,18 @@
 import { Client } from "discord.js";
 import UserAgent from "user-agents";
 import fs from "fs";
+import path from "path";
 import { NikeRestockAPIRequestData } from "../models/requests/NikeRestockAPIRequestData";
 import NikeRestockMonitorService from "../services/NikeRestockMonitorService";
 import NikeRestockMonitor from "./NikeRestockMonitor";
 import NikeRestockPuppeteerScrapeRepository from "../repositories/implementations/NikeRestockPuppeteerScrapeRepository";
 
 export default async function createRestockDropMonitor(discordClient: Client) {
-  const requestsObjects = fs.readFileSync(
-    "../../../../scrape_data/monitors/Nike/restocks.json",
-    "utf-8",
+  const requestsObjects = JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, "../../../../scrape_data/monitors/Nike/restocks.json"),
+      "utf-8",
+    ),
   ) as unknown as NikeRestockAPIRequestData[];
   const userAgent = new UserAgent({ deviceCategory: "desktop" });
   const nikeRestockMonitorService = new NikeRestockMonitorService();
