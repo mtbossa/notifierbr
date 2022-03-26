eb62c6b66b6ea88fc5a7336024c221fc4d4364bf
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-03-26 11:32:43 -0300

    move monitor file to monitors folderr

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { exitHandler } from "./helpers/general";
 import { configureDiscordBotClient } from "./discord-bot"; // Runs code when imported (bot.ts runs code when called)
 import createNikeFlashDropMonitor from "./monitors/Nike/FlashDropMonitor/NikeFlashDropMonitorFactory";
 import createRestockDropMonitor from "./monitors/Nike/RestockMonitor/NikeRestockMonitorFactory";
+import createArtwalkRestockMonitor from "./monitors/Artwalk/RestockMonitor/ArtwalkRestockMonitorFactory";
 
 const startNikeFlashDropsMonitors = async (discordClient: Client) => {
   const monitor = createNikeFlashDropMonitor(discordClient);
@@ -14,12 +15,19 @@ const startNikeRestockMonitors = async (discordClient: Client) => {
   monitor.start();
 };
 
+const startArtwalkRestockMonitors = async (discordClient: Client) => {
+  const monitor = await createArtwalkRestockMonitor(discordClient);
+  monitor.start();
+};
+
 // Starts all process
 (async () => {
   process.stdin.resume();
   process.on("SIGINT", exitHandler.bind(null, { exit: true }));
   const client = await configureDiscordBotClient();
 
-  startNikeFlashDropsMonitors(client);
-  startNikeRestockMonitors(client);
+  startArtwalkRestockMonitors(client);
+
+  // startNikeFlashDropsMonitors(client);
+  // startNikeRestockMonitors(client);
 })();
